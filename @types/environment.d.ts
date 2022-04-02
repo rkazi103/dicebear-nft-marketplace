cfbbf0025d78e3c2bad02a2d2b2aa31ca9142306
Rayan <rayankazi7515@gmail.com>
2022-04-02 10:45:38 -0700

    fix: fixed formatting issue studio

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
     NEXT_PUBLIC_SANITY_DATASET: string;
     NEXT_PUBLIC_SANITY_PROJECT_ID: string;
+    NEXT_PUBLIC_SANITY_DATASET: string;
   }
 }
