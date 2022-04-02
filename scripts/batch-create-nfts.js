5e7e7e72eb639d305f6c22294f0fc46c368bffaf
Rayan <rayankazi7515@gmail.com>
2022-04-02 12:07:06 -0700

    fix: renamed script

--- scripts/batch-create-nfts.js
+++ scripts/batch-create-nfts.js
@@ -6,9 +6,9 @@ const nftDrop = sdk.getNFTDrop("0x4FcFE963E1e40DCDf7f3F6b7260499FE673972a5");
 (async () => {
   try {
     await nftDrop.createBatch(nfts);
-    console.log("✅ Sucessfully NFTs minted.");
+    console.log("✅ Sucessfully created NFTS.");
   } catch (err) {
-    console.log("❌ Failed to mint NFTs");
+    console.log("❌ Failed to create NFTs");
     console.error(err);
     process.exit(1);
   }
