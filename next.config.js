cb4c20df6233e7b83c4a62eb9d4fe1eb8420d35a
Rayan <rayankazi7515@gmail.com>
2022-04-02 12:21:29 -0700

    chore: added sanity studio rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["avatars.dicebear.com"],
   },
+  rewrites: () => [STUDIO_REWRITE],
 };
